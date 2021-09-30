7b85d0bddfff9cf1464e44336f92a06136a81fef
Andre Mesquita <mesquitas@hotmail.com>
2021-09-29 23:56:28 -0300

    Update index.php

--- index.php
+++ index.php
@@ -5,7 +5,23 @@ $categorias []= 'infantil';
 $categorias [] = 'adolescentes';
 $categorias [] = 'adultos';
 $categorias [] = 'idoso';
+//print_r($categorias);
 
-print_r($categorias);
+$nome = 'Andre';
+$idade = 8;
 
+//var_dump($nome);
+//var_dump($idade);
 
+if ($idade >= 6 && $idade <= 12)
+{
+   echo 'Infantil';
+}
+else if ($idade == 13 && $idade == 18)
+{
+    echo 'Adolescente';
+}
+else
+{
+    echo 'Adulto';
+}
